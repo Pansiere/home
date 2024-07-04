0b1fd50a0d53426eaf6c7e0714b934fed93fd0aa
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-07-04 09:22:20 -0300

    update

---
+++ Aulas na Alura/6 - Consultas SQL avançando no SQL com MySQL/6 - select-condicoes.sql
@@ -0,0 +1,19 @@
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Manga';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Morango';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Morango' OR TAMANHO = '470 ml';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Manga' AND TAMANHO = '470 ml';
+
+SELECT * FROM tabela_de_produtos WHERE NOT (SABOR = 'Manga' AND TAMANHO = '470 ml');
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'Manga' AND NOT (TAMANHO = '470 ml');
+
+SELECT * FROM tabela_de_produtos WHERE SABOR IN ('Manga', 'Laranja');/* está ultimo comando é equivalente ao -> SELECT * FROM tabela_de_produtos WHERE SABOR = 'Manga' OR SABOR = 'Laranja'; */
+
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('Rio de Janeiro', 'São Paulo') AND IDADE >= 20;
+
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('Rio de Janeiro', 'São Paulo') AND (IDADE >= 19 AND IDADE <= 21);
+
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('Rio de Janeiro', 'São Paulo') AND (IDADE >= 19 AND IDADE <= 22);
