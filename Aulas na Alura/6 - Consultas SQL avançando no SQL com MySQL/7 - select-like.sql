fa60206cab8b8896311d53f9fff61eb6f7d34bf8
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-07-04 10:04:22 -0300

    update

---
+++ Aulas na Alura/6 - Consultas SQL avançando no SQL com MySQL/7 - select-like.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR LIKE '%Maça' AND EMBALAGEM = 'PET';
+
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Mattos'
---
+++ Aulas na Alura/6 - Consultas SQL avançando no SQL com MySQL/9 - select-limit.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tabela_de_produtos;
+
+SELECT * FROM tabela_de_produtos LIMIT 5;
+
+SELECT * FROM tabela_de_produtos LIMIT 2,3;
+
+SELECT * FROM notas_fiscais  WHERE DATA_VENDA = '2017-01-01' limit 10
