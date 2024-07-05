a5a9f917eeabfea756c861e0a0cd41d687fe6148
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-07-05 01:29:12 -0300

    update

--- Aulas na Alura/6 - Consultas SQL avançando no SQL com MySQL/19 - funcoes.sql
+++ Aulas na Alura/6 - Consultas SQL avançando no SQL com MySQL/19 - funcoes.sql
@@ -18,3 +18,6 @@ USE sucos_vendas;
 
 SELECT CONCAT(NOME, ' (', CPF, ') ') AS RESULTADO 
 FROM tabela_de_clientes;
+
+SELECT NOME, CONCAT(ENDERECO_1, ' ', BAIRRO, ' ', CIDADE, ' ', ESTADO) AS COMPLETO
+FROM tabela_de_clientes
