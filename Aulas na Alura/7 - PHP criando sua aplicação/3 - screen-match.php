b2e8c77fe130f784d253004286aeea1cda41bdbb
JP <ojp027@gmail.com>
2024-07-07 08:13:45 -0300

    update

---
+++ Aulas na Alura/7 - PHP criando sua aplicação/3 - screen-match.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Bem vindo(a) ao screen match!
+";
+
+$nomeFilme = "Top Guns - Maverick";
+$anoLancamento = 2022;
+$nota = 8.8;
+$inckuidoNoPlano = true;
+
+echo $nomeFilme;
---
+++ Aulas na Alura/7 - PHP criando sua aplicação/4 - string-numerica.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Bem vindo(a) ao screen match!\n";
+
+$nomeFilme = "Top Guns - Maverick";
+$anoLancamento = 2022;
+$nota = 8.8;
+$inckuidoNoPlano = true;
+
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nome do filme: $nomeFilme \n";
