3f6d66c94e0915cf9ccc81e95b4cc0e3210182e3
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-07-12 04:12:01 -0300

    update

--- Aulas na Alura/9 - Orientação a Objetos com PHP Classes, métodos e atributos/src/2 - CONTA.php
+++ Aulas na Alura/9 - Orientação a Objetos com PHP Classes, métodos e atributos/src/2 - CONTA.php
@@ -5,4 +5,22 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+        } else {
+            $this->saldo += $valorADepositar;
+        }
+    }
 }
