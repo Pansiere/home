f36561a8f62dccf61196aa31d096544e41b04285
JP <ojp027@gmail.com>
2024-07-17 15:58:50 -0300

    update

--- Aulas na Alura/9 - Orientação a Objetos com PHP Classes, métodos e atributos/1 - banco.php
+++ Aulas na Alura/9 - Orientação a Objetos com PHP Classes, métodos e atributos/1 - banco.php
@@ -7,5 +7,5 @@ $primeiraConta = new Conta('123.456.789-10', 'Vinicius Dias');
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300);
 
-echo $primeiraConta->recuperaSaldo();
-echo $primeiraConta->recuperaCpfTitular();
+echo "Saldo: R$" . $primeiraConta->recuperaSaldo();
+echo "\nCPF do titular: " . $primeiraConta->recuperaCpfTitular();
