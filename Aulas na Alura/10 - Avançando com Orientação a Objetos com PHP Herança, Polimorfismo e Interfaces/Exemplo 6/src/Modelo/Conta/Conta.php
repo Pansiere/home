5d63b3f2a79f4d1dc81ee667c412d7f00dc06aeb
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-07-24 23:50:59 -0300

    update

--- Aulas na Alura/10 - Avançando com Orientação a Objetos com PHP Herança, Polimorfismo e Interfaces/Exemplo 6/src/Modelo/Conta/Conta.php
+++ Aulas na Alura/10 - Avançando com Orientação a Objetos com PHP Herança, Polimorfismo e Interfaces/Exemplo 6/src/Modelo/Conta/Conta.php
@@ -7,18 +7,39 @@ class Conta
     private $titular;
     private $saldo;
     private static $numeroDeContas = 0;
+    /**
+     * @var int $tipo 1 = Conta Corrente; 2 = Poupança
+     */
+    private $tipo;
 
-    public function __construct(Titular $titular)
+    public function __construct(Titular $titular, int $tipo)
     {
         $this->titular = $titular;
         $this->saldo = 0;
-
+        $this->tipo = $tipo;
         self::$numeroDeContas++;
     }
 
     public function __destruct()
     {
         self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void
+    {
+        if ($this->tipo === 1) {
+            $tarifaSaque = $valorASacar * 0.05;
+        } else {
+            $tarifaSaque = $valorASacar * 0.03;
+        }
+
+        $valorSaque = $valorASacar + $tarifaSaque;
+        if ($valorSaque > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorSaque;
     }
 
     public function deposita(float $valorADepositar): void
---
+++ Aulas na Alura/10 - Avançando com Orientação a Objetos com PHP Herança, Polimorfismo e Interfaces/Exemplo 6/teste-sque.php
@@ -0,0 +1,21 @@
+<?php
+
+use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+
+require_once 'autoload.php';
+
+$conta = new Conta(
+    new Titular(
+        new CPF('123.456.789-10'),
+        'Vinicius Dias',
+        new Endereco('Petropolis', 'bairro Teste', 'Rua lá', '37'),
+    ),
+    2
+);
+
+$conta->deposita(500);
+$conta->saca(100);
+echo $conta->recuperaSaldo();
