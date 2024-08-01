0ec9283fdaeab4da8139b00276d43865bd053c19
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-08-01 05:43:46 -0300

    update

---
+++ Aulas na Alura/11 - Design Patterns em PHP padroes comportamentais/Exemplo 5/gera-pedido.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Alura\DesignPattern\{GerarPedido, GerarPedidoHandler, Orcamento, Pedido};
+
+$valorOrcamento = $argv[1];
+$numeroDeItens = $argv[2];
+$nomeCliente = $argv[3];
+
+$gerarPedido = new GerarPedido($valorOrcamento, $numeroDeItens, $nomeCliente);
+$gerarPedidoHandler = new GerarPedidoHandler();
+$gerarPedidoHandler->execute($gerarPedido);
