be8114764bf0a409b2165a6b2f9ccd8654e8c03b
João Pedro V. Pansiere <ojp027@gmail.com>
2024-08-10 16:21:41 -0300

    update

--- Aulas na Alura/14 - PHP e MySQL criando sua primeira aplicação web/2 - projeto/public/index.php
+++ Aulas na Alura/14 - PHP e MySQL criando sua primeira aplicação web/2 - projeto/public/index.php
@@ -1,10 +1,18 @@
+<?php include 'code.php'; ?>
+
 <!doctype html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="
+            width=device-width,
+            user-scalable=no,
+            initial-scale=1.0,
+            maximum-scale=1.0,
+            minimum-scale=1.0
+        ">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/index.css">
@@ -31,38 +39,16 @@
                 <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-cafe-manha-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-cremoso.jpg">
+                <?php foreach ($produtosCafe as $cafe): ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $cafe['imagem'] ?>">
+                        </div>
+                        <p><?= $cafe['nome'] ?></p>
+                        <p><?= $cafe['descricao'] ?></p>
+                        <p><?= "R$ " . $cafe['preco'] ?></p>
                     </div>
-                    <p>Café Cremoso</p>
-                    <p>Café cremoso irresistivelmente suave e que envolve seu paladar</p>
-                    <p>R$ 5.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-com-leite.jpg">
-                    </div>
-                    <p>Café com Leite</p>
-                    <p>A harmonia perfeita do café e do leite, uma experiência reconfortante</p>
-                    <p>R$ 2.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cappuccino.jpg">
-                    </div>
-                    <p>Cappuccino</p>
-                    <p>Café suave, leite cremoso e uma pitada de sabor adocicado</p>
-                    <p>R$ 7.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-gelado.jpg">
-                    </div>
-                    <p>Café Gelado</p>
-                    <p>Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.</p>
-                    <p>R$ 3.00</p>
-                </div>
+                <?php endforeach; ?>
             </div>
         </section>
         <section class="container-almoco">
@@ -71,40 +57,18 @@
                 <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-almoco-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/bife.jpg">
+                <?php foreach ($produtoAlmoco as $almoco): ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $almoco['imagem'] ?>">
+                        </div>
+                        <p><?= $almoco['nome'] ?></p>
+                        <p><?= $almoco['descricao'] ?></p>
+                        <p><?= "R$ " . $almoco['preco'] ?></p>
                     </div>
-                    <p>Bife</p>
-                    <p>Bife, arroz com feijão e uma deliciosa batata frita</p>
-                    <p>R$ 27.90</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-peixe.jpg">
-                    </div>
-                    <p>Filé de peixe</p>
-                    <p>Filé de peixe salmão assado, arroz, feijão verde e tomate.</p>
-                    <p>R$ 24.99</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-frango.jpg">
-                    </div>
-                    <p>Frango</p>
-                    <p>Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante</p>
-                    <p>R$ 23.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/fettuccine.jpg">
-                    </div>
-                    <p>Fettuccine</p>
-                    <p>Prato italiano autêntico da massa do fettuccine com peito de frango grelhado</p>
-                    <p>R$ 22.50</p>
-                </div>
+                <?php endforeach; ?>
             </div>
-
+            </div>
         </section>
     </main>
 </body>
