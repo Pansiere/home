fc03d513c86ee4a34b2690dfd2394809d7d9ef32
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-23 13:55:39 -0300

    update

--- Aulas na Alura/16 - PHP I O trabalhando com arquivos e streams/6 - Facilitando a escrita/escritor-cursos.php
+++ Aulas na Alura/16 - PHP I O trabalhando com arquivos e streams/6 - Facilitando a escrita/escritor-cursos.php
@@ -1,12 +1,5 @@
 <?php
 
-$arquivo = fopen('cursos-php.txt', 'a');
+$curso = "Design Patters PHP I: Boas práticas de programação";
 
-#$curso = 'Design Patters PHP I: Boas práticas de programação';
-$curso = "\nDesign Patters PHP II: Boas práticas de programação";
-
-fwrite($arquivo, $curso);
-#fwrite($arquivo, $curso, 20);
-
-
-fclose($arquivo);
+file_put_contents('cursos-php.txt', $curso);
