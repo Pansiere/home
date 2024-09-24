8e1f3eb5cf2b74283c1fd343bcb00a80e56d7d14
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-24 19:05:32 -0300

    update

--- Aulas na Alura/16 - PHP I O trabalhando com arquivos e streams/23 - mb_convert_encoding/spl.php
+++ Aulas na Alura/16 - PHP I O trabalhando com arquivos e streams/23 - mb_convert_encoding/spl.php
@@ -7,7 +7,11 @@ var_dump($arquivoCursos);
 while (!$arquivoCursos->eof()) {
     $linha = $arquivoCursos->fgetcsv(';');
 
-    echo utf8_encode($linha[0]) . PHP_EOL;
+    echo mb_convert_encoding(
+        $linha[0],
+        'UTF-8',
+        'Windows-1252'
+    ) . PHP_EOL;
 }
 
 $date = new DateTime();
