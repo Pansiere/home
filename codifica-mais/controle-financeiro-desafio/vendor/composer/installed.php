e055d02167808b38574d8c1181e69c046874b553
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-24 21:05:33 -0300

    update

---
+++ codifica-mais/controle-financeiro-desafio/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pansiere/controle_financeiro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce519a2b6571818f19038b028ff1dc2ae987e0f3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pansiere/controle_financeiro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce519a2b6571818f19038b028ff1dc2ae987e0f3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
