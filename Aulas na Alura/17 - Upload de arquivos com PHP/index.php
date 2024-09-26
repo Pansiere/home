8c1701e2e3142d004a60a7c5dbd788db78d5263c
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-25 23:57:18 -0300

    update

---
+++ Aulas na Alura/17 - Upload de arquivos com PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>upload</title>
+</head>
+
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="arquivo">Arquivo</label>
+        <input type="file" name="arquivo" id="arquivo">
+        <button>Enviar</button>
+    </form>
+</body>
+
+</html>
