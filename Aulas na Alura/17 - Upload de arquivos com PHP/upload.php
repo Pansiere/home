7be7121bce49596ff70ecce5efbaf65ad60e6c4d
João Pedro V. Pansiere <ojp027@gmail.com>
2024-09-26 00:06:29 -0300

    update

--- Aulas na Alura/17 - Upload de arquivos com PHP/upload.php
+++ Aulas na Alura/17 - Upload de arquivos com PHP/upload.php
@@ -1,3 +1,13 @@
 <?php
 
-var_dump("Olá");
+/*
+PRECISO sempre verificar se o arquivo
+esperado é VALIDO, não receber arquivos
+executáveis por upload, etc
+*/
+
+var_dump($_FILES);
+
+var_dump(move_uploaded_file($_FILES['arquivo']['tmp_name'], __DIR__ . '/a.pdf'));
+
+echo __DIR__;
