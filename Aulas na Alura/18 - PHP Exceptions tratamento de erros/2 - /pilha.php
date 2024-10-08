58b9706b266fdd494d48a4fb11292639105ac628
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-07 22:52:30 -0300

    update

--- Aulas na Alura/18 - PHP Exceptions tratamento de erros/2 - /pilha.php
+++ Aulas na Alura/18 - PHP Exceptions tratamento de erros/2 - /pilha.php
@@ -1,13 +1,13 @@
 <?php
 
-function funcao1()
+function funcao1(): void
 {
     echo 'Entrei na função 1' . PHP_EOL;
     funcao2();
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
-function funcao2()
+function funcao2(): void
 {
     echo 'Entrei na função 2' . PHP_EOL;
     for ($i = 1; $i <= 5; $i++) {
