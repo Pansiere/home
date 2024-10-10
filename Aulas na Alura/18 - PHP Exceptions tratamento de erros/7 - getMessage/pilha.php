f06f6ffbd40525e9c8ce0c1b5201b24c1efca0c4
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-10 01:36:16 -0300

    update

---
+++ Aulas na Alura/18 - PHP Exceptions tratamento de erros/7 - getMessage/pilha.php
@@ -0,0 +1,34 @@
+<?php
+
+function funcao1(): void
+{
+    echo 'Entrei na função 1' . PHP_EOL;
+
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $problema) {
+        echo $problema->getMessage() . PHP_EOL;
+        echo $problema->getLine() . PHP_EOL;
+        echo $problema->getTraceAsString() . PHP_EOL;
+    }
+
+    echo 'Saindo da função 1' . PHP_EOL;
+}
+
+function funcao2(): void
+{
+    echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    $divicao = intdiv(5, 0);
+
+    for ($i = 1; $i <= 5; $i++) {
+        echo $i . PHP_EOL;
+    }
+    echo 'Saindo da função 2' . PHP_EOL;
+}
+
+echo 'Iniciando o programa principal' . PHP_EOL;
+funcao1();
+echo 'Finalizando o programa principal' . PHP_EOL;
