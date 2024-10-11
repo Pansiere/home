98464bda747b9f180e62b29eb0f40c33e858cf1f
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-11 11:24:28 -0300

    update

--- Aulas na Alura/19 - laravel/1 - controle-series/routes/web.php
+++ Aulas na Alura/19 - laravel/1 - controle-series/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get(uri: '/', action: function (): mixed {
     return view('welcome');
+});
+
+Route::get(uri: '/ola', action: function (): void {
+    echo 'Olá, Mundo!';
 });
