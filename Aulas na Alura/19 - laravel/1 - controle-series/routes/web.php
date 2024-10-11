b41bb3ad51982eefb81c887c4db2e1ec04a625d8
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-11 12:12:57 -0300

    update

--- Aulas na Alura/19 - laravel/1 - controle-series/routes/web.php
+++ Aulas na Alura/19 - laravel/1 - controle-series/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 
-Route::get(uri: '/', action: function (): mixed {
-    return view('welcome');
-});
+// Route::get(uri: '/', action: function (): mixed {
+//     return view('welcome');
+// });
 
 Route::get(uri: '/ola', action: [SeriesController::class, 'index']);
