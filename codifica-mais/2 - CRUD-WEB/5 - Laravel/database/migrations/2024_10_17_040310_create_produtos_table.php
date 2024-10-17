c50e6660d1e6ad0916d7b4ecf3ee5e301088d3f6
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-10-17 01:18:07 -0300

    update

--- codifica-mais/2 - CRUD-WEB/5 - Laravel/database/migrations/2024_10_17_040310_create_produtos_table.php
+++ codifica-mais/2 - CRUD-WEB/5 - Laravel/database/migrations/2024_10_17_040310_create_produtos_table.php
@@ -9,16 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->string('imagem');
             $table->string('nome');
             $table->string('sku')->unique();
             $table->decimal('valor', 8, 2);
             $table->integer('quantidade');
+            $table->unsignedBigInteger('unidade_medida_id');
+            $table->unsignedBigInteger('categoria_id');
             $table->timestamps();
+
+            $table->foreign('unidade_medida_id')->references('id')->on('unidades_medida')->onDelete('cascade');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
         });
     }
 
