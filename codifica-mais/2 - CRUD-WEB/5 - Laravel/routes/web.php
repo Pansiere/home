fe9e353168d407d88523226eb06e8782689c05ae
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-17 20:48:15 -0300

    update

--- codifica-mais/2 - CRUD-WEB/5 - Laravel/routes/web.php
+++ codifica-mais/2 - CRUD-WEB/5 - Laravel/routes/web.php
@@ -3,16 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::get('/', [ProdutoController::class, 'index']);
-
-Route::get('/criar', [ProdutoController::class, 'criar']);
-
-Route::get('/editar', [ProdutoController::class, 'editar']);
-
-Route::post('/salvar', [ProdutoController::class, 'salvar']);
-
-Route::post('/deletar', [ProdutoController::class, 'deletar']);
-
-Route::post('/atualizar', [ProdutoController::class, 'atualizar']);
+Route::resource('produtos', ProdutoController::class,);
 
 Route::post('/uploadCsv', [ProdutoController::class, 'uploadCsv']);
