f232b7afde5fab949c4cdeb983d939451cce3b60
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-10-18 01:02:33 -0300

    update

--- codifica-mais/2 - CRUD-WEB/5 - Laravel/resources/views/components/layout.blade.php
+++ codifica-mais/2 - CRUD-WEB/5 - Laravel/resources/views/components/layout.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    "resources/css/global.css",
     @vite(['resources/js/app.js', 'resources/css/listagem.css', 'resources/css/formulario.css', 'resources/css/global.css'])
     <title>{{$title}}</title>
 </head>
