4a69ea23945a4f208c62d27bc7507f13d0eba543
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-18 17:51:14 -0300

    update

--- codifica-mais/2 - CRUD-WEB/5 - Laravel/database/migrations/2024_10_17_130405_create_produtos_table.php
+++ codifica-mais/2 - CRUD-WEB/5 - Laravel/database/migrations/2024_10_17_130405_create_produtos_table.php
@@ -31,64 +31,6 @@ return new class extends Migration
             $table->foreign('unidade_medida_id')->references('id')->on('unidades_medidas')->onDelete('cascade');
             $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
         });
-
-        DB::table('produtos')->insert([
-            [
-                'imagem' => 'imagem/teste',
-                'nome' => 'Smartphone',
-                'sku' => 'SP001',
-                'valor' => 1999.99,
-                'quantidade' => 10,
-                'unidade_medida_id' => 1,
-                'categoria_id' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'imagem' => 'imagem/teste',
-                'nome' => 'Geladeira',
-                'sku' => 'GLD001',
-                'valor' => 3599.90,
-                'quantidade' => 5,
-                'unidade_medida_id' => 1,
-                'categoria_id' => 2,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'imagem' => 'imagem/teste',
-                'nome' => 'Cadeira de Escritório',
-                'sku' => 'CAD001',
-                'valor' => 499.90,
-                'quantidade' => 25,
-                'unidade_medida_id' => 1,
-                'categoria_id' => 3,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'imagem' => 'imagem/teste',
-                'nome' => 'Camiseta',
-                'sku' => 'CAM001',
-                'valor' => 49.90,
-                'quantidade' => 100,
-                'unidade_medida_id' => 1,
-                'categoria_id' => 5,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'imagem' => 'imagem/teste',
-                'nome' => 'Televisor 55"',
-                'sku' => 'TV001',
-                'valor' => 2499.00,
-                'quantidade' => 12,
-                'unidade_medida_id' => 1,
-                'categoria_id' => 1,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        ]);
     }
 
     /**
