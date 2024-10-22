feeba3af281c51e8f41ab187411f5da9a494d030
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-10-22 14:03:29 -0300

    update

--- Aulas na Alura/20 - Laravel -  validando formulários, usando sessões e definindo relacionamentos/resources/views/series/create.blade.php
+++ Aulas na Alura/20 - Laravel -  validando formulários, usando sessões e definindo relacionamentos/resources/views/series/create.blade.php
@@ -1,13 +1,3 @@
 <x-layout title="Nova Série">
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    <x-series.form :action="route('series.store')" />
-
+    <x-series.form :action="route('series.store')" :nome="old('nome')" :update="false" />
 </x-layout>
