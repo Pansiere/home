558b701ba4931bf740fc7a935a8cd1fa628573cc
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-27 06:28:17 -0300

    update

--- app controle-financeiro/Laravel/routes/web.php
+++ app controle-financeiro/Laravel/routes/web.php
@@ -2,14 +2,24 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    DashboardController,
+    TransacoesController,
+    CategoriasController,
+    UsuarioController
+};
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('dashboard') : to_route('login');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('/transacao', TransacoesController::class);
+
+Route::resource('/categorias', CategoriasController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +27,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
