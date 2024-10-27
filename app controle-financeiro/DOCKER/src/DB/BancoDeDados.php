30ba153b249062c52f6e82721d205922f06303fe
João Pedro V. Pansiere <ojp027@gmail.com>
2024-10-26 22:03:21 -0300

    update

--- app controle-financeiro/DOCKER/src/DB/BancoDeDados.php
+++ app controle-financeiro/DOCKER/src/DB/BancoDeDados.php
@@ -9,13 +9,15 @@ class BancoDeDados
     public static function ConexaoDB(): PDO
     {
         try {
+            $config = require __DIR__ . '/../config.php';
+
             $conexao = new PDO(
-                dsn: 'mysql:host=127.0.0.1;dbname=controle_financeiro',
-                username: 'controle_financeiro_user',
-                password: 'CODIFICAMAIS'
+                dsn: 'mysql:host=' . $config['DB_HOST'] . ';dbname=' . $config['DB_NAME'],
+                username: $config['DB_USER'],
+                password: $config['DB_PASS']
             );
-            $conexao->setAttribute(PDO::ATTR_ERRMODE,  PDO::ERRMODE_EXCEPTION);
-            $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,  PDO::FETCH_ASSOC);
+            $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             return $conexao;
         } catch (\PDOException $e) {
