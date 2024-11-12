379d5a24e57a027255c79a503246e819017060ed
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-12 17:54:19 -0300

    update

---
+++ Aulas na Alura/Trainee/1 - Laravel transações, service container e autenticação/2 - Injecao de dependencia/app/Repositories/SeriesRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+//use App\Http\Requests\SeriesFormRequest;
+//use App\Models\Series;
+
+interface SeriesRepository
+{
+//    public function add(SeriesFormRequest $request): Series;
+
+    const bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+}
