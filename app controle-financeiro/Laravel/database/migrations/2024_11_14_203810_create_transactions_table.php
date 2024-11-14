2348805cb8afa48777706eee4876e7717d571b5c
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-14 17:52:45 -0300

    update

---
+++ app controle-financeiro/Laravel/database/migrations/2024_11_14_203810_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('type', ['Despesa', 'Receita']);
+            $table->decimal('value', 10, 2);
+            $table->string('description');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
