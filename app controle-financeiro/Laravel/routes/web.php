dfbcc6376ce4bc07a3a470557b19eac03c4e6d97
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-14 10:25:43 -0300

    update

--- app controle-financeiro/Laravel/routes/web.php
+++ app controle-financeiro/Laravel/routes/web.php
@@ -1,25 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{DashboardController, TransacoesController, CategoriasController};
 
 Route::get('/', function () {
-    return to_route('dashboard');
+    return view('welcome');
 });
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::resource('/transacao', TransacoesController::class)
-    ->middleware(['auth', 'verified'])->name('dsad,', 'teste');
-
-Route::resource('/categorias', CategoriasController::class)
-    ->middleware(['auth', 'verified']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
