9b85500454eba50e1e18c1b23110618386ce54fd
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-27 10:31:57 -0300

    update

--- app controle-financeiro/DOCKER/config.exemplo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'controle_financeiro',
-    'DB_USER' => 'controle_financeiro_user',
-    'DB_PASS' => 'CODIFICAMAIS'
-];
--- app controle-financeiro/DOCKER/src/Controller/Controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Pansiere\ControleFinanceiro\Controller;
-
-abstract class Controller {}
